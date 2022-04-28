09b49bc6b9edbe8b240145d0382f46f11a7dd50f
Chriserus <krzysborowiecki97@gmail.com>
2022-04-28 13:50:22 +0200

    Remove getTimeRemaining

--- src/app/common/common.ts
+++ src/app/common/common.ts
@@ -5,34 +5,6 @@ export const getBuildingTypeName = (buildingType: BuildingType): string => {
   return BuildingType[buildingType];
 };
 
-export const getTimeRemaining = (end: string) => {
-  const total = new Date(+end * 1000).getTime() - new Date().getTime();
-  let seconds: string | number = Math.floor((total / 1000) % 60);
-  let minutes: string | number = Math.floor((total / 1000 / 60) % 60);
-  let hours: string | number = Math.floor((total / (1000 * 60 * 60)) % 24);
-  let days: string | number = Math.floor((total / (1000 * 60 * 60 * 24)));
-  if (seconds < 10) {
-    seconds = String(seconds).padStart(2, '0');
-  }
-  if (minutes < 10) {
-    minutes = String(minutes).padStart(2, '0');
-  }
-  if (hours < 10) {
-    hours = String(hours).padStart(2, '0');
-  }
-  if (days < 10) {
-    days = String(days).padStart(2, '0');
-  }
-
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-};
-
 export const _filter = (value: number, options: number[]): string[] => {
   if (!value) {
     return options.map(option => option.toString());
