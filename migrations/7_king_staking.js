7d6f55dd2cee59dd9009863c4dc269ac1ef3db36
Chriserus <krzysborowiecki97@gmail.com>
2022-06-17 12:22:24 +0200

    Dev env corrections

--- migrations/7_king_staking.js
+++ migrations/7_king_staking.js
@@ -1,13 +1,14 @@
 const production = require('../src/assets/addresses/production.ts')
 const test = require('../src/assets/addresses/test.ts')
-const development = require('../src/assets/addresses/development.ts')
 const {deployProxy} = require('@openzeppelin/truffle-upgrades');
 const Village = artifacts.require('Village');
 const KingStaking = artifacts.require('KingStaking');
+const KingToken = artifacts.require('KingToken');
 module.exports = async function (deployer, network) {
   const village = await Village.deployed();
   if (network === "development") {
-    await deployProxy(KingStaking, [village.address, development.kingAddress], {deployer});
+    const kingToken = await KingToken.deployed();
+    await deployProxy(KingStaking, [village.address, kingToken.address], {deployer});
   } else if (network === 'bsctestnet' || network === 'bsctestnet-fork') {
     await deployProxy(KingStaking, [village.address, test.kingAddress], {deployer});
   } else if (network === 'bscmainnet' || network === 'bscmainnet-fork') {
