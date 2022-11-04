4cf32b9cc0190c68819bcf6c2ae727026fca3a3c
Chriserus <krzysborowiecki97@gmail.com>
2022-11-04 14:23:07 +0100

    Undefined fix

--- src/assets/addresses/development.ts
+++ src/assets/addresses/development.ts
@@ -15,9 +15,9 @@ if (process.env["NODE_ENV"] === "development") {
 const networkId = '5777';
 
 module.exports = {
-  cbkLandAddress: CBKLand.networks[networkId]?.address,
-  charactersAddress: Characters.networks[networkId]?.address,
-  weaponsAddress: Weapons.networks[networkId]?.address,
-  skillAddress: SkillToken.networks[networkId]?.address,
-  garrisonAddress: Garrison.networks[networkId]?.address,
+  cbkLandAddress: CBKLand?.networks[networkId]?.address,
+  charactersAddress: Characters?.networks[networkId]?.address,
+  weaponsAddress: Weapons?.networks[networkId]?.address,
+  skillAddress: SkillToken?.networks[networkId]?.address,
+  garrisonAddress: Garrison?.networks[networkId]?.address,
 }
